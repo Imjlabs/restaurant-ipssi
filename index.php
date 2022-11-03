9a0b661e027c4bd72a396a914d3ebd06b332fe3f
jonathan-gu <jonathanguerpillon@gmail.com>
2022-11-03 15:05:53 +0100

    template index.php

--- index.php
+++ index.php
@@ -1,25 +1,4 @@
 <?php
 
-if (!isset($_GET['action'])) {
-    require_once "View/index.php";
-}
-else {
-    if ($_GET['action'] == "menu") {
-        require_once "View/menu.php";
-    }
-    else if ($_GET['action'] == "history") {
-        require_once "View/history.php";
-    }
-    else if ($_GET['action'] == "reservation") {
-        require_once "View/reservation.php";
-    }
-    else if ($_GET['action'] == "contact") {
-        require_once "View/contact.php";
-    }
-    else if ($_GET['action'] == "connection") {
-        require_once "View/connection.php";
-    }
-    else if ($_GET['action'] == "registration") {
-        require_once "View/registration.php";
-    }
-}
+define("ROOT", __DIR__);
+require ROOT. "/vendor/autoload.php";
