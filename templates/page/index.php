1b771c1d3e18aef60e3be168ba23bf4661dea863
imjlabs <jordan.raphael@outlook.fr>
2022-11-06 12:18:05 +0100

    "Error images"

--- templates/page/index.php
+++ templates/page/index.php
@@ -1,302 +1,281 @@
 <section class="home-banner-area relative">
-		<div class="container">
-			<div class="row height align-items-center justify-content-center">
-				<div class="home-banner-content col-lg-5">
-					<h1> <p> KUET    Restaurant </p></h1>
-					<hr>
-					<p>A Fine Delicate Restaurant</p>
-				</div>
+	<div class="container">
+		<div class="row height align-items-center justify-content-center">
+			<div class="home-banner-content col-lg-5">
+				<h1>
+					<p>Restaurant IPSSI</p>
+					
+				</h1>
+				<hr>
 			</div>
 		</div>
-	</section>
-	<!--================ End banner Area =================-->
-
-	<!--================ Left Right And Banner Icon =================-->
-	<div class="go-down">
-		<a href="#menu_area">
-			<img class="go-down-img" src="img/go-down.png" alt="">
-		</a>
 	</div>
-	<div class="fixed-view-menu">
-		<p>
-			<a href="menu.html">view menu</a>
-		</p>
+</section>
+
+
+
+<div class="go-down">
+	<a href="#menu_area">
+		<img class="go-down-img" src="./src/pictures/go-down.png" alt="">
+	</a>
+</div>
+<div class="fixed-view-menu">
+	<p>
+		<a href="menu.html">Voir le menu</a>
+	</p>
+</div>
+<div class="fixed-book-table">
+	<p>
+		<a href="booking.html">Résersvation d'une table</a>
+	</p>
+</div>
+
+
+<section class="menu-area" id="menu_area">
+	<div class="container">
+		<div class="row align-items-center">
+
+			<div class="col-lg-11 col-md-10">
+				<div class="menu-list">
+					<div class="single-menu">
+						<h3>Apéritif</h3>
+						<ul class="list">
+							<li>
+								<p class="menu-item">Nachos<span> 5 euros</span></p>
+								<p>(Nachos 1:2)</p>
+							</li>
+							<li>
+								<p class="menu-item">Battonet de poulet croustillant<span> 8 euros</span></p>
+								<p>Battonet de poulet avec frites</p>
+							</li>
+							<li>
+								<p class="menu-item">Pain à l'ail<span> 6 euros</span> </p>
+								<p>Pain à l'ail avec fromage</p>
+
+							</li>
+
+							<li>
+								<p class="menu-item">Poulet en sauce<span> 10 euros</span> </p>
+								<p> Poulet en sauce avec purée</p>
+
+							</li>
+
+
+						</ul>
+					</div>
+
+					<div class="single-menu">
+						<h3>Set Menu</h3>
+						<ul class="list">
+							<li>
+								<p class="menu-item">Poulet au piment avec frites<span> 11 euros</span></p>
+
+							</li>
+							<li>
+								<p class="menu-item">Brochette de poulet,légumes et boissons<span>1</span> 15 euros</p>
+
+							</li>
+							<li>
+								<p class="menu-item">Poulet Barbecue, riz et légumes<span> 17 euros</span></p>
+
+							</li>
+							<li>
+								<p class="menu-item">Poulet sucrée et doux avec riz<span> 12 euros</span></p>
+
+							</li>
+
+
+						</ul>
+					</div>
+
+					<div class="single-menu">
+						<h3>Jus Frais et Milk-shake</h3>
+						<ul class="list">
+							<li>
+								<p class="menu-item">Milk-shake au chocolat<span> 6 euros</span></p>
+
+							</li>
+							<li>
+								<p class="menu-item">Milkshake à la vanille<span> 6 euros</span></p>
+
+							</li>
+							<li>
+								<p class="menu-item">Milkshake oreo<span> 5.50 euros</span></p>
+
+							</li>
+							<li>
+								<p class="menu-item">Jus de pomme avec canelle<span> 7 euros</span></p>
+
+							</li>
+							<li>
+								<p class="menu-item">Jus d'ananas<span> 5 euros</span></p>
+
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
-	<div class="fixed-book-table">
-		<p>
-			<a href="booking.html">book a table</a>
-		</p>
+</section>
+
+<section class="gallery-area">
+	<div class="container">
+		<div class="row align-items-center">
+			<div class="col-lg-6 col-md-5 col-sm-6">
+				<div class="single-gallery mt--480">
+					<div class="overlay"></div>
+					<figure>
+						<img src="img/gallery/elishpolao.jpg" class="img-fluid w-100" alt="">
+					</figure>
+					<div class="icon">
+						<a href="img/gallery/elishpolao.jpg" class="photo-gallery-pop-up">
+							<span class="lnr lnr-cross"></span>
+						</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-5 col-md-6 col-sm-6">
+				<div class="single-gallery">
+					<div class="overlay"></div>
+					<figure>
+						<img src="./src/pictures/g3.jpg" class="img-fluid w-100" alt="">
+					</figure>
+					<div class="icon">
+						<a href="./src/pictures/g3.jpg" class="photo-gallery-pop-up">
+							<span class="lnr lnr-cross"></span>
+						</a>
+					</div>
+				</div>
+			</div>
+
+			<div class="offset-lg-1 col-lg-5 col-md-5 col-sm-6">
+				<div class="single-gallery mt--280">
+					<div class="overlay"></div>
+					<figure>
+						<img src="./src/pictures/pizza.jpg" class="img-fluid w-100" alt="">
+					</figure>
+					<div class="icon">
+						<a href="./src/pictures/pizza.jpg" class="photo-gallery-pop-up">
+							<span class="lnr lnr-cross"></span>
+						</a>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-lg-5 col-md-6 col-sm-6">
+				<div class="single-gallery">
+					<div class="overlay"></div>
+					<figure>
+						<img src="./src/pictures/khichuri.jpg" class="img-fluid w-100" alt="">
+					</figure>
+					<div class="icon">
+						<a href="./src/pictures/khichuri.jpg" class="photo-gallery-pop-up">
+							<span class="lnr lnr-cross"></span>
+						</a>
+					</div>
+				</div>
+			</div>
+
+			<div class="offset-lg-1 col-lg-5 col-md-5 col-sm-6">
+				<div class="single-gallery mt--280">
+					<div class="overlay"></div>
+					<figure>
+						<img src="./src/pictures/puja.jpg" class="img-fluid w-100" alt="">
+					</figure>
+					<div class="icon">
+						<a href="./src/pictures/puja.jpg" class="photo-gallery-pop-up">
+							<span class="lnr lnr-cross"></span>
+						</a>
+					</div>
+				</div>
+			</div>
+
+
+
+			<div class="col-lg-6 col-md-6 col-sm-6">
+				<div class="single-gallery">
+					<div class="overlay"></div>
+					<figure>
+						<img src="./src/pictures/g4.jpg" class="img-fluid w-100" alt="">
+					</figure>
+					<div class="icon">
+						<a href="./src/pictures/g4.jpg" class="photo-gallery-pop-up">
+							<span class="lnr lnr-cross"></span>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
-	<!--================ Left Right And Banner Icon =================-->
+</section>
 
-	<!--================ Menu Area =================-->
-	<section class="menu-area" id="menu_area">
-		<div class="container">
-			<div class="row align-items-center">
-			
-				<div class="col-lg-11 col-md-10">
-					<div class="menu-list">
-						<div class="single-menu">
-							<h3>Appetizer</h3>
-							<ul class="list">
-								<li>
-									<p class="menu-item">Nachos <span>190.00 tk</span></p>
-									<p>(Nachos 1:2)</p>
-								</li>
-								<li>
-									<p class="menu-item">Crirpsy Stick Chicken <span> 200.00tk</span></p>
-									<p>(Cripsy Stick Chicken with French fry 14 pcs)</p>
-								</li>
-								<li>
-							 <p  class="menu-item"> Garlic Bread <span> 150.00 tk</span> </p>
-							 <p> ( garlic bread with chesse 2 pcs)</p>
+<section class="chefs-quotes-area">
+	<div class="container">
+		<div class="row align-items-center">
+			<div class="col-lg-5 col-md-6">
+				<div class="section-title relative">
+					<h1>
+						Phrases<br>
+						du chef
+					</h1>
+					<p>
+					<h2>Un bon chef doit ètre un manager. Trvaillez avec passion pour produire d'excellent plat.</h2>
 
-								</li>
 
-								<li>
-									<p  class="menu-item"> Chicken Lolipop <span> 200.00 tk</span> </p>
-									<p> ( Chicken Lolipop 6 pcs)</p>
-	   
-									   </li>
-									   
+					</p>
+					<img src="./src/pictures/signature.png" class="img-fluid" alt="">
+				</div>
+			</div>
+			<div class="offset-lg-1 col-lg-6 col-md-6">
+				<div class="mt--120">
+					<img class="chef-img img-fluid" src="img/chef.jpg" alt="">
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
 
-							</ul>
+<section class="container section-gap-top">
+	<div class="callto-action-area relative">
+		<div class="row d-flex justify-content-center">
+			<div class="col-lg-12 p-0">
+				<div class="cta-owl owl-carousel">
+					<div class="item">
+						<div class="cta-img">
+							<img src="./src/pictures/callaction-bg.jpg" class="img-fluid" alt="">
 						</div>
+						<div class="text-box text-center">
 
-						<div class="single-menu">
-							<h3>Set Menu</h3>
-							<ul class="list">
-								<li>
-									<p class="menu-item">Chicken Chili with Fried Rice :<span>200.00tk </span></p>
-									
-								</li>
-								<li>
-									<p class="menu-item">Chicken Brochette,Vegetables and Soft Drinks : <span>250.00tk</span></p>
-									
-								</li>
-								<li>
-									<p class="menu-item">BBQ Chicken,Fried Rice with Vegetables : <span>210.00tk</span></p>
-									
-								</li>
-								<li>
-									<p class="menu-item">Sweet & Sugar Chicken with Fried Rice : <span>180.00tk</span></p>
-									
-								</li>
-								
-
-							</ul>
+							<p>
+								Steak au poulet avec pain à l'ail et frite
+							</p>
 						</div>
+					</div>
 
-						<div class="single-menu">
-							<h3>Fresh Juices & Milk Shake</h3>
-							<ul class="list">
-								<li>
-									<p class="menu-item">Choclate Milk Shake :<span>130.00tk</span></p>
-									
-								</li>
-								<li>
-									<p class="menu-item">Vanila Milk Shake :<span>110.00tk</span></p>
-								
-								</li>
-								<li>
-									<p class="menu-item">Orio Shake :<span>100.00tk</span></p>
-								
-								</li>
-								<li>
-									<p class="menu-item">Lassi :<span>90.00tk</span></p>
-								
-								</li>
-								<li>
-									<p class="menu-item">Pineapple Juiccce :<span>150.00tk</span></p>
-								
-								</li>
-							</ul>
+					<div class="item">
+						<div class="cta-img">
+							<img src="./src/pictures/burger.jpg" class="img-fluid" alt="">
+						</div>
+						<div class="text-box text-center">
+
+							<p>
+								Burger épicé
+							</p>
+						</div>
+					</div>
+
+					<div class="item">
+
+						<div class="text-box text-center">
+
+							<p>
+								Burgers
+							</p>
 						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-	</section>
-	<!--================End Menu Area =================-->
-
-	<!--================ Gallery Area =================-->
-	<section class="gallery-area">
-		<div class="container">
-			<div class="row align-items-center">
-				<div class="col-lg-6 col-md-5 col-sm-6">
-					<div class="single-gallery mt--480">
-						<div class="overlay"></div>
-						<figure>
-							<img src="img/gallery/elishpolao.jpg" class="img-fluid w-100" alt="">
-						</figure>
-						<div class="icon">
-							<a href="img/gallery/elishpolao.jpg" class="photo-gallery-pop-up">
-								<span class="lnr lnr-cross"></span>
-							</a>
-						</div>
-					</div>
-				</div>
-				<div class="col-lg-5 col-md-6 col-sm-6">
-					<div class="single-gallery">
-						<div class="overlay"></div>
-						<figure>
-							<img src="img/gallery/g3.jpg" class="img-fluid w-100" alt="">
-						</figure>
-						<div class="icon">
-							<a href="img/gallery/g3.jpg" class="photo-gallery-pop-up">
-								<span class="lnr lnr-cross"></span>
-							</a>
-						</div>
-					</div>
-				</div>
-
-				<div class="offset-lg-1 col-lg-5 col-md-5 col-sm-6">
-					<div class="single-gallery mt--280">
-						<div class="overlay"></div>
-						<figure>
-							<img src="img/gallery/pizza.jpg" class="img-fluid w-100" alt="">
-						</figure>
-						<div class="icon">
-							<a href="img/gallery/pizza.jpg" class="photo-gallery-pop-up">
-								<span class="lnr lnr-cross"></span>
-							</a>
-						</div>
-					</div>
-				</div>
-
-				<div class="col-lg-5 col-md-6 col-sm-6">
-					<div class="single-gallery">
-						<div class="overlay"></div>
-						<figure>
-							<img src="img/gallery/khichuri.jpg" class="img-fluid w-100" alt="">
-						</figure>
-						<div class="icon">
-							<a href="img/gallery/khichuri.jpg" class="photo-gallery-pop-up">
-								<span class="lnr lnr-cross"></span>
-							</a>
-						</div>
-					</div>
-				</div>
-
-				<div class="offset-lg-1 col-lg-5 col-md-5 col-sm-6">
-					<div class="single-gallery mt--280">
-						<div class="overlay"></div>
-						<figure>
-							<img src="img/gallery/puja.jpg" class="img-fluid w-100" alt="">
-						</figure>
-						<div class="icon">
-							<a href="img/gallery/puja.jpg" class="photo-gallery-pop-up">
-								<span class="lnr lnr-cross"></span>
-							</a>
-						</div>
-					</div>
-				</div>
-
-
-
-				<div class="col-lg-6 col-md-6 col-sm-6">
-					<div class="single-gallery">
-						<div class="overlay"></div>
-						<figure>
-							<img src="img/gallery/g4.jpg" class="img-fluid w-100" alt="">
-						</figure>
-						<div class="icon">
-							<a href="img/gallery/g4.jpg" class="photo-gallery-pop-up">
-								<span class="lnr lnr-cross"></span>
-							</a>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</section>
-	<!--================ End Gallery Area =================-->
-
-	<!--================End Reservation Area =================-->
-
-	<!--================ Chefs Quotes Area =================-->
-	<section class="chefs-quotes-area">
-		<div class="container">
-			<div class="row align-items-center">
-				<div class="col-lg-5 col-md-6">
-					<div class="section-title relative">
-						<h1>
-							Chef’s <br>
-							Quotes
-						</h1>
-						<p>
-							<h2> A good chef has to be a manager, a businessman and a great cook. To marry all three together is sometimes difficult.  </h2>
-								
-							
-						</p>
-						<img src="img/signature.png" class="img-fluid" alt="">
-					</div>
-				</div>
-				<div class="offset-lg-1 col-lg-6 col-md-6">
-					<div class="mt--120">
-						<img class="chef-img img-fluid" src="img/chef.jpg" alt="">
-					</div>
-				</div>
-			</div>
-		</div>
-	</section>
-	<!--================ End Chefs Quotes Area =================-->
-
-	<!--================ Start Call To Action Area =================-->
-	<section class="container section-gap-top">
-		<div class="callto-action-area relative">
-			<div class="row d-flex justify-content-center">
-				<div class="col-lg-12 p-0">
-					<div class="cta-owl owl-carousel">
-						<div class="item">
-							<div class="cta-img">
-								<img src="img/callaction-bg.jpg" class="img-fluid" alt="">
-							</div>
-							<div class="text-box text-center">
-							
-								<p>
-									Chicken Steak with gerlic bread & Fries
-								</p>
-							</div>
-						</div>
-
-						<div class="item">
-								<div class="cta-img">
-									<img src="img/burger.jpg" class="img-fluid" alt="">
-								</div>
-								<div class="text-box text-center">
-							
-									<p>
-										Spicy Blast Burger
-									</p>
-								</div>
-							</div>
-
-							<div class="item">
-									<div class="cta-img">
-										<img src="img/pasta2.jpg" class="img-fluid" alt="">
-									</div>
-									<div class="text-box text-center">
-								
-										<p>
-											KUET Resturant Special Pasta
-										</p>
-									</div>
-								</div>
-
-						<div class="item">
-							<div class="cta-img">
-								<img src="img/pizza.jpg" class="img-fluid" alt="">
-							</div>
-							<div class="text-box text-center">
-							
-								<p>
-									Mexian Chesse Pizza
-								</p>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</section>
+	</div>
+</section>
