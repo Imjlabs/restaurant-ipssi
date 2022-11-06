2c8a2f83e67dc6266dce617451b4ee937772aaf7
jonathan-gu <jonathanguerpillon@gmail.com>
2022-11-06 11:26:38 +0100

    style nav / footer

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -8,7 +8,15 @@ class PageController extends DefaultController {
     public function index() {
         $this->render("page/index", null, "Accueil", 
         [
-            '<link rel="stylesheet" href="./src/css/index.css">'
+            '<link rel="stylesheet" href="./src/css/linearicons.css">',
+            '<link rel="stylesheet" href="./src/css/font-awesome.min.css">',
+            '<link rel="stylesheet" href="./src/css/availability-calendar.css">',
+            '<link rel="stylesheet" href="./src/css/magnific-popup.css">',
+            '<link rel="stylesheet" href="./src/css/nice-selec.css">',
+            '<link rel="stylesheet" href="./src/css/owl.carousel.css">',
+            '<link rel="stylesheet" href="./src/css/bootstrap.css">',
+            '<link rel="stylesheet" href="./src/css/bootstrap-datepicker.css">',
+            '<link rel="stylesheet" href="./src/css/main.css">'
         ]);
     }
 }
--- templates/base.php
+++ templates/base.php
@@ -18,11 +18,14 @@
 	<title> KUET Restaurant</title>
 
 	<link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,400i|Roboto:400,500" rel="stylesheet">
+    <?php foreach ($links as $link) : ?>
+        <?= $link ?>
+    <?php endforeach ?>
 	<!--
 			CSS
 			============================================= -->
 
-	<link rel="stylesheet" href="css/linearicons.css">
+	<!-- <link rel="stylesheet" href="css/linearicons.css">
 	<link rel="stylesheet" href="css/font-awesome.min.css">
 	<link rel="stylesheet" href="css/availability-calendar.css">
 	<link rel="stylesheet" href="css/magnific-popup.css">
@@ -30,7 +33,7 @@
 	<link rel="stylesheet" href="css/owl.carousel.css">
 	<link rel="stylesheet" href="css/bootstrap.css">
 	<link rel="stylesheet" href="css/bootstrap-datepicker.css">
-	<link rel="stylesheet" href="css/main.css">
+	<link rel="stylesheet" href="css/main.css"> -->
 </head>
 
 <body>
