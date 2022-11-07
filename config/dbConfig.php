51b4cd781f404c00621e97ba8fa212395e2974e6
jonathan-gu <jonathanguerpillon@gmail.com>
2022-11-07 12:34:59 +0100

    OK

--- config/dbConfig.php
+++ config/dbConfig.php
@@ -2,7 +2,7 @@
 
 $dbConfig = [
     'host' => "localhost:3306",
-    'db' => "restaurant_ipssi",
+    'db' => "restaurant_ipssi_bdd",
     'user' => "root",
     'password' => ""
 ];
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -9,7 +9,7 @@ class User {
     private string $password;
     private string $phone_number;
     private int $status;
-    private bool $admin;
+    private int $admin;
     private string $created_at;
     private string $updated_at;
 
